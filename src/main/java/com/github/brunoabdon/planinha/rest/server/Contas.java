0e9e659b16ace2b839e955520861f50d55b6bb27
Bruno Abdon <brunoabdon+github@gmail.com>
2019-07-16 23:58:50 -0300

    Adiciona metodo de listar contas

--- src/main/java/com/github/brunoabdon/planinha/rest/server/Contas.java
+++ src/main/java/com/github/brunoabdon/planinha/rest/server/Contas.java
@@ -1,7 +1,14 @@
 package com.github.brunoabdon.planinha.rest.server;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 
 import com.github.brunoabdon.commons.dal.Dao;
 import com.github.brunoabdon.commons.rest.AbstractRestCrud;
@@ -14,7 +21,7 @@ public class Contas extends AbstractRestCrud<Conta, Integer> {
 
 	public static final String PATH = "contas";
 	
-	private Dao<Conta, Integer> dao;
+	private ContasDao dao;
 	
 	public Contas() {
 		super(PATH);
@@ -25,5 +32,16 @@ public class Contas extends AbstractRestCrud<Conta, Integer> {
 	protected Dao<Conta, Integer> getDao() {
 		return this.dao;
 	}
+	
+    @GET
+    public Response listar(
+            final @Context Request request,
+            final @Context HttpHeaders httpHeaders) {
+        
+        final List<Conta> contas = dao.listar(entityManager);
+
+        return super.buildResponse(request,httpHeaders,contas);
+    }
+
 
 }
