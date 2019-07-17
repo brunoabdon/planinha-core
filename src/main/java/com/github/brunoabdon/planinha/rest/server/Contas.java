6dfe9dddb733e18c7b493c045eabd9d7e8af44ed
Bruno Abdon <brunoabdon+github@gmail.com>
2019-07-16 23:24:58 -0300

    Servico Contas funcionando

--- src/main/java/com/github/brunoabdon/planinha/rest/server/Contas.java
+++ src/main/java/com/github/brunoabdon/planinha/rest/server/Contas.java
@@ -1,23 +1,29 @@
 package com.github.brunoabdon.planinha.rest.server;
 
+import javax.ejb.Stateless;
 import javax.ws.rs.Path;
 
 import com.github.brunoabdon.commons.dal.Dao;
 import com.github.brunoabdon.commons.rest.AbstractRestCrud;
 import com.github.brunoabdon.gastoso.Conta;
+import com.github.brunoabdon.gastoso.dal.ContasDao;
 
+@Stateless
 @Path(Contas.PATH)
 public class Contas extends AbstractRestCrud<Conta, Integer> {
 
 	public static final String PATH = "contas";
 	
+	private Dao<Conta, Integer> dao;
+	
 	public Contas() {
 		super(PATH);
+		this.dao = new ContasDao();
 	}
 
 	@Override
 	protected Dao<Conta, Integer> getDao() {
-		return null; // gastoso dao tá em gastoso-srv...
+		return this.dao;
 	}
 
 }
