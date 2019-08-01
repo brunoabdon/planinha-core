02ff7705b5aec9d18d34284569489ca8eb6b3453
Bruno Abdon <brunoabdon+github@gmail.com>
2019-08-01 16:00:51 -0300

    Usando um context-roo

--- src/main/java/com/github/brunoabdon/planinha/planinhacore/rest/RestApplication.java
+++ src/main/java/com/github/brunoabdon/planinha/planinhacore/rest/RestApplication.java
@@ -3,7 +3,7 @@ package com.github.brunoabdon.planinha.planinhacore.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/")
+@ApplicationPath("/planinha")
 public class RestApplication extends Application {
 
 }
