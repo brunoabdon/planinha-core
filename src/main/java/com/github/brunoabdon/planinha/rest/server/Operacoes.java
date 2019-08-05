ff3c9085b443a5b90447c0c0d9e19fda8344d883
Bruno Abdon <brunoabdon+github@gmail.com>
2019-08-05 14:34:39 -0300

    Adapta resource de Operacao

--- src/main/java/com/github/brunoabdon/planinha/rest/server/Operacoes.java
+++ src/main/java/com/github/brunoabdon/planinha/rest/server/Operacoes.java
@@ -1,81 +1,38 @@
 package com.github.brunoabdon.planinha.rest.server;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
 
-import org.jboss.logging.Logger;
-import org.jboss.logging.Logger.Level;
-
-import com.github.brunoabdon.commons.dal.DalException;
-import com.github.brunoabdon.gastoso.Fato;
-import com.github.brunoabdon.gastoso.Lancamento;
-import com.github.brunoabdon.gastoso.dal.FatosDao;
-import com.github.brunoabdon.gastoso.dal.LancamentosDao;
-import com.github.brunoabdon.planinha.Movimentacao;
+import com.github.brunoabdon.commons.dal.Dao;
+import com.github.brunoabdon.commons.rest.AbstractRestCrud;
 import com.github.brunoabdon.planinha.Operacao;
+import com.github.brunoabdon.planinha.OperacaoDao;
 
 @Path(Operacoes.PATH)
-public class Operacoes {
+public class Operacoes extends AbstractRestCrud<Operacao, Integer>{
 
-	@Inject
-	private Logger logger;
-	
+    private static final ResponseBuilder NOT_ALLOWED = 
+        Response.status(Status.METHOD_NOT_ALLOWED);
+
     protected static final String PATH = "operacoes";
-    
-    @PersistenceContext
-    protected EntityManager entityManager;
-    
-    private final FatosDao fatosDao;
-    private final LancamentosDao lancamentosDao;
+
+	private final OperacaoDao operacoesDao;
     
     public Operacoes() {
-    	this.fatosDao = new FatosDao(); //pq não são singletons?
-    	this.lancamentosDao = new LancamentosDao();
+        super(PATH);
+    	this.operacoesDao = new OperacaoDao();
 	}
-        
-	
-    @POST
-    @Transactional
-    public Response criar(final Operacao operacao) 
-    		throws URISyntaxException {
-    	
-    	ResponseBuilder rb;
-    	
-    	try {
-			final Fato fato = operacao.getFato();
-			this.fatosDao.criar(entityManager, fato);
-			
-			for (final Movimentacao movimentacao:operacao.getMovimentacoes()){
-				final Lancamento lancamento = 
-					new Lancamento(
-						fato,
-						movimentacao.getConta(), 
-						movimentacao.getValor()
-					);
-				this.lancamentosDao.criar(entityManager, lancamento);
-			}
-			
-            final URI uri = new URI(PATH + fato.getId());
-			
-			rb = Response.created(uri).entity(operacao);
-			
-			
-		} catch (final DalException e) {
-            logger.logv(Level.ERROR, e, "Erro ao tentar criar {0}.", operacao);
-            rb = 
-                Response
-                	.status(Response.Status.CONFLICT)
-                    .entity(e.getMessage());
-		}
-    	return rb.build();
+
+    @Override
+    protected Dao<Operacao, Integer> getDao() {
+        return this.operacoesDao;
+    }
+
+    @Override
+    @SuppressWarnings("unused")
+    public Response atualizar(final Integer id, final Operacao entity) {
+        return NOT_ALLOWED.build();
     }
 }
