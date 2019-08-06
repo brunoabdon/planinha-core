0f2925536fdefc4f72b8eed85b3e1243738065b9
Bruno Abdon <brunoabdon+github@gmail.com>
2019-08-06 18:39:47 -0300

    Cria resource de Extratos

---
+++ src/main/java/com/github/brunoabdon/planinha/rest/server/Extratos.java
@@ -0,0 +1,27 @@
+package com.github.brunoabdon.planinha.rest.server;
+
+import javax.ws.rs.Path;
+
+import com.github.brunoabdon.commons.dal.Dao;
+import com.github.brunoabdon.commons.rest.AbstractRestReadOnlyResource;
+import com.github.brunoabdon.planinha.Extrato;
+import com.github.brunoabdon.planinha.Extrato.Id;
+import com.github.brunoabdon.planinha.dal.ExtratosDao;
+
+@Path(Extratos.PATH)
+public class Extratos extends AbstractRestReadOnlyResource<Extrato, Id> {
+
+    protected static final String PATH = "extratos";
+
+    private ExtratosDao extratosDao;
+    
+    public Extratos() {
+        this.extratosDao = new ExtratosDao();
+    }
+
+    @Override
+    protected Dao<Extrato, Id> getDao() {
+        return this.extratosDao;
+    }
+
+}
