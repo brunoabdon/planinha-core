f83d058d841d055d3764be82fd2cbeff509bcfea
Bruno Abdon <brunoabdon+github@gmail.com>
2019-08-07 19:01:23 -0300

    Cria resource de lancamentos

---
+++ src/main/java/com/github/brunoabdon/planinha/rest/server/Lancamentos.java
@@ -0,0 +1,39 @@
+package com.github.brunoabdon.planinha.rest.server;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.github.brunoabdon.commons.rest.AbstractRestCrud;
+import com.github.brunoabdon.gastoso.Lancamento;
+import com.github.brunoabdon.gastoso.dal.LancamentosDao;
+
+/**
+ *
+ * @author bruno
+ */
+@Path(Lancamentos.PATH)
+@Produces(MediaType.APPLICATION_JSON)
+public class Lancamentos extends AbstractRestCrud<Lancamento, Integer> {
+
+    protected static final String PATH = "lancamentos";
+            
+    private final LancamentosDao dao;
+
+    public Lancamentos() {
+        super(PATH);
+        this.dao = new LancamentosDao();
+    }
+
+    @Override
+    protected LancamentosDao getDao() {
+        return dao;
+    }
+    
+    @Override
+    protected void prencheValoresAtualizacao(
+            final Lancamento lancamentoPraAtualizar,
+            final Lancamento lancamentoValoresNovos) {
+        lancamentoPraAtualizar.setValor(lancamentoValoresNovos.getValor());
+    }
+}
