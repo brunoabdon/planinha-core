37e1fbd89083e881211758e4db1150bbcf6c3e5a
Bruno Abdon <brunoabdon+github@gmail.com>
2019-10-21 23:54:59 -0300

    funcionando com o primeiro commit do branch bug-classloader do abd-commons

--- src/main/java/com/github/brunoabdon/planinha/rest/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package com.github.brunoabdon.planinha.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/planinha")
-public class RestApplication extends Application {
-
-}
