4d30ba9cac3bc76bfa514bf9706d4f9710458429
Bruno Abdon <brunoabdon+github@gmail.com>
2019-11-22 00:13:08 -0300

    Cria filtros

---
+++ src/main/java/com/github/brunoabdon/planinha/facade/filtro/FiltroConta.java
@@ -0,0 +1,9 @@
+package com.github.brunoabdon.planinha.facade.filtro;
+
+import java.io.Serializable;
+
+public class FiltroConta implements Serializable {
+
+    private static final long serialVersionUID = 1765786932474241272L;
+
+}
---
+++ src/main/java/com/github/brunoabdon/planinha/facade/filtro/FiltroOperacao.java
@@ -0,0 +1,9 @@
+package com.github.brunoabdon.planinha.facade.filtro;
+
+import java.io.Serializable;
+
+public class FiltroOperacao implements Serializable{
+
+    private static final long serialVersionUID = -2823304175074337261L;
+
+}
