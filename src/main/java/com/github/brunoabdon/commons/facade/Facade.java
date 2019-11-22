6f29a5fd89dd4d45a2fe9783ccd6040580075cbd
Bruno Abdon <brunoabdon+github@gmail.com>
2019-11-22 02:30:53 -0300

    Adiciona @Validacao na Facade

--- src/main/java/com/github/brunoabdon/commons/facade/Facade.java
+++ src/main/java/com/github/brunoabdon/commons/facade/Facade.java
@@ -3,23 +3,28 @@ package com.github.brunoabdon.commons.facade;
 import java.util.List;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import com.github.brunoabdon.commons.modelo.Identifiable;
 
 public interface Facade <X extends Identifiable<K>,K,F,A>{
 
     @Transactional(rollbackOn={RuntimeException.class,BusinessException.class})
-    public X cria(final X elemento) throws BusinessException;
+    public X cria(@NotNull @Valid final X elemento) throws BusinessException;
     
-    public X pega(final K key) throws EntidadeInexistenteException;
+    public X pega(@NotNull @Valid final K key) 
+        throws EntidadeInexistenteException;
     
     public List<X> listar(final F filtro);
     
     @Transactional(rollbackOn={RuntimeException.class,BusinessException.class})
-    public X atualiza(final K key, final A atualizacao) 
+    public X atualiza(@
+            NotNull @Valid final K key, 
+            @NotNull @Valid final A atualizacao) 
         throws EntidadeInexistenteException, BusinessException;
 
     @Transactional(rollbackOn={RuntimeException.class,BusinessException.class})
-    public void deleta(final K key) 
+    public void deleta(@NotNull @Valid final K key) 
         throws EntidadeInexistenteException, BusinessException;
 }
