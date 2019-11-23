44cb676fc9e4242791de2d0e9951315bb1b96c0f
Bruno Abdon <brunoabdon+github@gmail.com>
2019-11-23 02:22:58 -0200

    Cria utilitário de java.time

---
+++ src/main/java/com/github/brunoabdon/planinha/util/TimeUtils.java
@@ -0,0 +1,72 @@
+package com.github.brunoabdon.planinha.util;
+
+import static java.time.temporal.TemporalAdjusters.firstDayOfMonth;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * Métodos utilitários nem tão utilitários mas úteis pra deixar os métodos de
+ * negócio mais legíveis. E mais testáveis.
+ * 
+ * @author bruno
+ *
+ */
+@ManagedBean
+@ApplicationScoped
+public class TimeUtils {
+
+    private static final TemporalAdjuster NO_INICIO_DO_MES = firstDayOfMonth();
+
+    private static final TemporalAdjuster NO_FIM_DO_MES = 
+        TemporalAdjusters.lastDayOfMonth();
+
+    private static final UnaryOperator<LocalDate> PASSA_PRO_MES_SEGUINTE = 
+        d -> d.plusMonths(1);
+        
+    public LocalDate noInicioDoMes(final LocalDate dia) {
+        return dia.with(NO_INICIO_DO_MES);
+    }
+
+    public LocalDate noFimDoMes(final LocalDate dia) {
+        return dia.with(NO_FIM_DO_MES);
+    }
+    
+    public long quantosMesesEntre(
+            final LocalDate primeiroDia, 
+            final LocalDate ultimoDia) {
+        return Period.between(primeiroDia, ultimoDia).toTotalMonths();
+    }
+    
+    public Stream<LocalDate> streamMensal(final LocalDate dia){
+        final LocalDate diaInicial = noInicioDoMes(dia);
+
+        return Stream.iterate(diaInicial, PASSA_PRO_MES_SEGUINTE);
+    }
+
+    public Stream<LocalDate> streamMensal(
+            final LocalDate diaInicio, 
+            final LocalDate diaFinal){
+
+        long quantidadeDeMeses = quantosMesesEntre(diaInicio, diaFinal);
+        
+        return streamMensal(diaInicio).limit(quantidadeDeMeses);
+    }
+    
+    public Stream<LocalDate> streamMensalAteHoje(
+            final LocalDate diaInicio){
+
+        final LocalDate hoje = LocalDate.now();
+
+        long quantidadeDeMeses = quantosMesesEntre(diaInicio, hoje);
+        
+        return streamMensal(diaInicio).limit(quantidadeDeMeses);
+    }
+}
