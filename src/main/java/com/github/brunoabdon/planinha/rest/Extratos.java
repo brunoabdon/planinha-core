c3ba3b4418d72bfe482dffe216dab3bbc9e1b11f
Bruno Abdon <brunoabdon+github@gmail.com>
2019-12-11 23:54:11 -0300

    Implementa pegar extrato

--- src/main/java/com/github/brunoabdon/planinha/rest/Extratos.java
+++ src/main/java/com/github/brunoabdon/planinha/rest/Extratos.java
@@ -17,7 +17,10 @@ import org.jboss.logging.Logger;
 import com.github.brunoabdon.commons.facade.EntidadeInexistenteException;
 import com.github.brunoabdon.planinha.facade.ContaFacade;
 import com.github.brunoabdon.planinha.facade.ExtratoFacade;
+import com.github.brunoabdon.planinha.modelo.Conta;
 import com.github.brunoabdon.planinha.modelo.Extrato;
+import com.github.brunoabdon.planinha.modelo.Extrato.Id;
+import com.github.brunoabdon.planinha.modelo.Periodo;
 
 @Path("contas/{conta_id}/extratos")
 public class Extratos {
@@ -45,5 +48,20 @@ public class Extratos {
         }
 
         return Response.ok(extratosDaConta).build();
+    }
+    
+    @GET
+    @Path("{periodo}")
+    @Produces(APPLICATION_JSON)
+    public Response pegar(
+            @PathParam("conta_id") final Integer idConta,
+            @PathParam("periodo") final Periodo periodo) 
+                throws EntidadeInexistenteException {
+        
+        final Extrato.Id id = new Id(new Conta(idConta), periodo);
+
+        final Extrato extrato = facade.pega(id);
+        
+        return Response.ok(extrato).build();
     }
 }
