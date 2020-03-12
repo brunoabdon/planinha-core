9c51a8717fbb7bc187319d15dc65333228d1e5e4
Bruno Abdon <brunoabdon+guthub@gmail.com>
2020-03-12 03:13:32 -0300

    Corrige exceção

--- src/main/java/com/github/brunoabdon/commons/facade/EntidadeInexistenteException.java
+++ src/main/java/com/github/brunoabdon/commons/facade/EntidadeInexistenteException.java
@@ -7,14 +7,14 @@ public class EntidadeInexistenteException extends Exception {
     private static final long serialVersionUID = 2749804844713036560L;
 
     private static final String ERRO_NOT_FOUND = 
-        "com.github.brunoabdon.commons.notfound.%s";
+        "com.github.brunoabdon.commons.notfound.%s.%s";
 
     private final Serializable id;
     
     public EntidadeInexistenteException(
             final Class<?> tipo, 
             final Serializable id) {
-        super(String.format(ERRO_NOT_FOUND));
+        super(String.format(ERRO_NOT_FOUND,tipo.getName(),id));
         this.id = id;
     }
     
