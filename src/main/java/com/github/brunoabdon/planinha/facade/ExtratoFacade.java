6fbf2a294790ffddcfae481352b3dd53562dcaa8
Bruno Abdon <brunoabdon+guthub@gmail.com>
2020-03-12 03:16:33 -0300

    Consulta extratos

--- src/main/java/com/github/brunoabdon/planinha/facade/ExtratoFacade.java
+++ src/main/java/com/github/brunoabdon/planinha/facade/ExtratoFacade.java
@@ -2,6 +2,7 @@ package com.github.brunoabdon.planinha.facade;
 
 import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toList;
+import static org.jboss.logging.Logger.Level.DEBUG;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -11,13 +12,19 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.jboss.logging.Logger;
+
 import com.github.brunoabdon.commons.facade.BusinessException;
 import com.github.brunoabdon.commons.facade.EntidadeInexistenteException;
 import com.github.brunoabdon.commons.facade.Facade;
+import com.github.brunoabdon.planinha.dal.ExtratoConsulta;
 import com.github.brunoabdon.planinha.dal.FatoConsulta;
+import com.github.brunoabdon.planinha.dal.ItemConsulta;
+import com.github.brunoabdon.planinha.filtro.FiltroItem;
 import com.github.brunoabdon.planinha.modelo.Conta;
 import com.github.brunoabdon.planinha.modelo.Extrato;
 import com.github.brunoabdon.planinha.modelo.Extrato.Id;
+import com.github.brunoabdon.planinha.modelo.Extrato.Item;
 import com.github.brunoabdon.planinha.modelo.Periodo;
 import com.github.brunoabdon.planinha.util.TimeUtils;
 
@@ -25,6 +32,9 @@ import com.github.brunoabdon.planinha.util.TimeUtils;
 public class ExtratoFacade
         implements Facade<Extrato, Extrato.Id, Integer, Void> {
 
+	@Inject 
+	Logger logger;
+	
     @PersistenceContext
     EntityManager em;
     
@@ -32,10 +42,16 @@ public class ExtratoFacade
     FatoConsulta fatoConsulta;
     
     @Inject
+    ExtratoConsulta extratoConsulta;
+    
+    @Inject
     TimeUtils tmu;
         
     @Inject 
     ContaFacade contaFacade;
+    
+    @Inject 
+    ItemConsulta itemConsulta;
     
     @Override
     public Extrato cria(final Extrato elemento) throws BusinessException {
@@ -44,8 +60,26 @@ public class ExtratoFacade
 
     @Override
     public Extrato pega(final Id key) throws EntidadeInexistenteException {
-        // TODO Auto-generated method stub
-        return null;
+
+    	logger.logv(DEBUG, "Pegando extrato de id {0}.", key);
+    	
+		final Conta conta = contaFacade.pega(key.getConta().getId());
+		final Periodo periodo = key.getPeriodo();
+		final LocalDate dataInicial = periodo.getDataMinima();
+		
+		final int saldoAnterior = 
+			extratoConsulta.saldoNoInicioDoDia(conta, dataInicial);
+		
+		final FiltroItem filtroItem = new FiltroItem();
+		
+		filtroItem.setConta(conta.getId());
+		
+		filtroItem.setDataMinima(periodo.getDataMinima());
+		filtroItem.setDataMaxima(periodo.getDataMaxima());
+		
+		final List<Item> itens = itemConsulta.listar(filtroItem);
+		
+		return new Extrato(new Id(conta, periodo), saldoAnterior, itens);
     }
 
     @Override
