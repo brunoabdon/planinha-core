09ca928f5457167f8850ff90befa614428650d9c
Bruno Abdon <brunoabdon+guthub@gmail.com>
2020-03-12 03:15:55 -0300

    Cria consulta pra saldo da conta

---
+++ src/main/java/com/github/brunoabdon/planinha/dal/ExtratoConsulta.java
@@ -0,0 +1,40 @@
+package com.github.brunoabdon.planinha.dal;
+
+import java.time.LocalDate;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.jboss.logging.Logger;
+
+import com.github.brunoabdon.commons.dal.CriteriaUtils;
+import com.github.brunoabdon.planinha.modelo.Conta;
+
+@ApplicationScoped
+public class ExtratoConsulta {
+    
+	
+	@Inject
+    Logger logger;
+    
+    @PersistenceContext
+    EntityManager em;
+    
+    @Inject
+    CriteriaUtils critUtils;
+	public int saldoNoInicioDoDia(final Conta conta, final LocalDate dia) {
+		
+		return 
+			em.createNamedQuery(
+				"Lancamento.saldoDaContaNoInicioDoDia",
+				Number.class
+			).setParameter("conta", conta)
+			 .setParameter("dia", dia)
+			 .getResultStream()
+			 .findFirst()
+			 .orElse(0)
+			 .intValue();
+	}
+}
