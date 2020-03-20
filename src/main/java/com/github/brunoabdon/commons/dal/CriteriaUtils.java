fc705a166c9cf0a62515cf82042332649d7952a5
Bruno Abdon <brunoabdon+guthub@gmail.com>
2020-03-19 21:53:09 -0300

    Corrige método between

--- src/main/java/com/github/brunoabdon/commons/dal/CriteriaUtils.java
+++ src/main/java/com/github/brunoabdon/commons/dal/CriteriaUtils.java
@@ -13,66 +13,70 @@ import javax.persistence.criteria.Predicate;
 public class CriteriaUtils {
 
 	private static final Optional<Predicate> NONE = Optional.empty();
-	
+
 	public <X> Optional<Predicate> eq(
-			final CriteriaBuilder cb, 
+			final CriteriaBuilder cb,
 			final Expression<X> exp,
 			final X valor){
-    	
+
 		return op(cb::equal,exp, valor);
 	}
 
 	public <X extends Comparable<? super X>> Optional<Predicate> greaterThan(
-			final CriteriaBuilder cb, 
+			final CriteriaBuilder cb,
 			final Expression<X> exp,
 			final X valor){
-    	
+
 		return op(cb::greaterThan,exp, valor);
 	}
-	
-	public <X extends Comparable<? super X>> Optional<Predicate> 
+
+	public <X extends Comparable<? super X>> Optional<Predicate>
 				greaterThanOrEqualTo(
-					final CriteriaBuilder cb, 
+					final CriteriaBuilder cb,
 					final Expression<X> exp,
 					final X valor){
-    	
+
 		return op(cb::greaterThanOrEqualTo,exp, valor);
 	}
-	
+
 	public <X extends Comparable<? super X>> Optional<Predicate> lessThan(
-			final CriteriaBuilder cb, 
+			final CriteriaBuilder cb,
 			final Expression<X> exp,
 			final X valor){
-    	
+
 		return op(cb::lessThan,exp, valor);
 	}
-	
-	public <X extends Comparable<? super X>> Optional<Predicate> 
+
+	public <X extends Comparable<? super X>> Optional<Predicate>
 				lessThanOrEqualTo(
-					final CriteriaBuilder cb, 
+					final CriteriaBuilder cb,
 					final Expression<X> exp,
 					final X valor){
-    	
+
 		return op(cb::lessThanOrEqualTo,exp, valor);
 	}
-	
+
 	public <X extends Comparable<? super X>> Optional<Predicate> between(
-			final CriteriaBuilder cb, 
+			final CriteriaBuilder cb,
 			final Expression<X> exp,
 			final X valor1,
 			final X valor2){
-    	
-		return 
+
+		return
 			valor1 != null && valor2 != null
-				? Optional.of(cb.between(exp, valor1, valor2)) 
-				: NONE;
+				? Optional.of(cb.between(exp, valor1, valor2))
+				: valor1 != null
+					? greaterThanOrEqualTo(cb, exp, valor1)
+					: valor2 != null
+						? lessThanOrEqualTo(cb, exp, valor2)
+						: NONE;
 	}
-	
+
 	private <X> Optional<Predicate> op(
 			final BiFunction<Expression<X>, X, Predicate> op,
 			final Expression<X> exp,
 			final X valor){
-    	
+
 		return valor != null ? Optional.of(op.apply(exp, valor)) : NONE;
 	}
 }
