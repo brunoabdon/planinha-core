0af74fddd4dcb7a96dcf9de8c0942b80c18320cc
Bruno Abdon <brunoabdon+guthub@gmail.com>
2021-04-11 19:18:08 -0300

    Limpa

--- src/main/java/com/github/brunoabdon/planinha/dal/modelo/virtual/SaldoInicial.java
+++ src/main/java/com/github/brunoabdon/planinha/dal/modelo/virtual/SaldoInicial.java
@@ -11,7 +11,6 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
-import javax.persistence.PrimaryKeyJoinColumn;
 
 import org.hibernate.annotations.Subselect;
 
