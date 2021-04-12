1fbecb7dbfbfa317ca5a8b7c94dfffeb28746faa
Bruno Abdon <brunoabdon+guthub@gmail.com>
2021-04-12 16:48:00 -0300

    Permite consulta por perídoos não fechados

--- src/main/java/com/github/brunoabdon/planinha/dal/FatoRepository.java
+++ src/main/java/com/github/brunoabdon/planinha/dal/FatoRepository.java
@@ -1,8 +1,12 @@
 package com.github.brunoabdon.planinha.dal;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 import java.time.LocalDate;
 import java.util.stream.Stream;
 
+import org.slf4j.Logger;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import com.github.brunoabdon.commons.dal.Repositorio;
@@ -13,16 +17,41 @@ import com.github.brunoabdon.planinha.modelo.Periodo;
 public interface FatoRepository extends Repositorio<Fato, Integer> {
 
 
+    public static final Logger log = getLogger(FatoRepository.class);
+
     public default Stream<Fato> findByPeriodo(
             final Periodo periodo) {
-        return
-            this.findByDiaBetween(
-                periodo.getInicio(),
-                periodo.getFim()
-            );
+
+        log.debug("Pesquisando fatos pelo perído {}.", periodo);
+
+        final Stream<Fato> fatos;
+
+        final  LocalDate inicio = periodo.getInicio();
+        final  LocalDate fim = periodo.getFim();
+
+        if(periodo.isFechado()){
+            log.trace("Pesquisando fatos com dia entre {} e {}.", inicio, fim);
+            fatos = this.findByDiaBetween(inicio,fim);
+        } else if(Periodo.SEMPRE.equals(periodo)) {
+            log.trace("Pesquisando fatos em qualquer dia.");
+            fatos = findAll(Pageable.unpaged()).stream();
+        } else if(inicio != null){
+            log.trace("Pesquisando fatos a partir do dia {}.", inicio);
+            fatos = findByDiaGreaterThanEqual(inicio);
+        } else {
+            log.trace("Pesquisando fatos até o dia {}.", fim);
+            fatos = findByDiaLessThanEqual(fim);
+        }
+        return fatos;
     }
+
+    public Stream<Fato> findByDiaGreaterThanEqual(final LocalDate dia);
+
+    public Stream<Fato> findByDiaLessThanEqual(final LocalDate dia);
 
     public Stream<Fato> findByDiaBetween(
             final LocalDate dataMinima,
             final LocalDate dataMaxima);
+
+
 }
