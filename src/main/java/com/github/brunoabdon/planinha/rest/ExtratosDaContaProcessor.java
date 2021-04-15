33d868edea6a90bcf922b169243ba0d2b6f547fe
Bruno Abdon <brunoabdon+guthub@gmail.com>
2021-04-15 19:38:41 -0300

    Incluir extrato da conta no json

---
+++ src/main/java/com/github/brunoabdon/planinha/rest/ExtratosDaContaProcessor.java
@@ -0,0 +1,27 @@
+package com.github.brunoabdon.planinha.rest;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+import org.springframework.hateoas.server.RepresentationModelProcessor;
+import org.springframework.stereotype.Component;
+
+import com.github.brunoabdon.planinha.rest.model.ContaModel;
+
+//TODO agrupar os Controlers com os Processors em pacotes.
+@Component
+public class ExtratosDaContaProcessor
+            implements RepresentationModelProcessor<ContaModel> {
+
+    @Override
+    public ContaModel process(final ContaModel model) {
+
+        final Integer idConta = model.getId();
+
+        model.add(
+            linkTo(Extratos.class, idConta).withRel("extratos")
+        );
+
+        return model;
+    }
+
+}
