3269f63fb4e7b96ffda051d3fbde68dc9682d6e3
Bruno Abdon <brunoabdon+github@gmail.com>
2021-08-22 00:36:20 -0300

    MOdels

---
+++ src/main/java/com/github/brunoabdon/planinha/rest/model/MovimentacaoModel.java
@@ -0,0 +1,28 @@
+package com.github.brunoabdon.planinha.rest.model;
+
+import java.io.Serializable;
+
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Relation(collectionRelation = "movimentacoes", itemRelation = "movimentacao")
+public class MovimentacaoModel extends RepresentationModel<MovimentacaoModel> implements Serializable {
+
+    private static final long serialVersionUID = -7901188884085707429L;
+
+    @EqualsAndHashCode.Exclude
+    private int valor;
+
+    @EqualsAndHashCode.Exclude
+    private ContaModel conta;
+
+}
---
+++ src/main/java/com/github/brunoabdon/planinha/rest/model/OperacaoModel.java
@@ -0,0 +1,33 @@
+package com.github.brunoabdon.planinha.rest.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Relation(collectionRelation = "operacoes", itemRelation = "operacao")
+public class OperacaoModel extends RepresentationModel<OperacaoModel>
+        implements Serializable {
+
+    private static final long serialVersionUID = 3807782862001395553L;
+
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    @EqualsAndHashCode.Exclude
+    private FatoModel fato;
+
+    @EqualsAndHashCode.Exclude
+    private List<MovimentacaoModel> movimentacoes;
+
+}
