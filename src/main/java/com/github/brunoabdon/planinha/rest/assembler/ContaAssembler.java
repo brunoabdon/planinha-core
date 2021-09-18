c324961c94738ffda135f3174c12a4a3438c7b50
bruno abdon <brunoabdon@github.com>
2021-09-18 00:56:21 -0300

    Corrige classe controller usada

--- src/main/java/com/github/brunoabdon/planinha/rest/assembler/ContaAssembler.java
+++ src/main/java/com/github/brunoabdon/planinha/rest/assembler/ContaAssembler.java
@@ -3,6 +3,7 @@ package com.github.brunoabdon.planinha.rest.assembler;
 import com.github.brunoabdon.commons.rest.assembler.IdentifiableModelAssembler;
 import com.github.brunoabdon.commons.rest.modelfiller.ModelFiller;
 import com.github.brunoabdon.planinha.modelo.ContaVO;
+import com.github.brunoabdon.planinha.rest.Contas;
 import com.github.brunoabdon.planinha.rest.model.ContaModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,8 @@ import org.springframework.stereotype.Component;
 public class ContaAssembler extends IdentifiableModelAssembler<ContaVO, ContaModel> {
 
     @Autowired
-    protected ContaAssembler(final ModelFiller<ContaModel,ContaVO> filler) {
-        super(ContaVO.class,ContaModel.class,filler);
+    protected ContaAssembler(
+            final ModelFiller<ContaModel,ContaVO> filler) {
+        super(Contas.class, ContaModel.class, filler);
     }
 }
