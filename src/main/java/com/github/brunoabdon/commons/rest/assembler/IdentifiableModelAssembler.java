4eb0341880d5a1bdd9b16c6c4e966e8f46b852c4
bruno abdon <brunoabdon@github.com>
2021-09-18 02:58:40 -0300

    Acrescenta debug a IdenfiableAssembler

--- src/main/java/com/github/brunoabdon/commons/rest/assembler/IdentifiableModelAssembler.java
+++ src/main/java/com/github/brunoabdon/commons/rest/assembler/IdentifiableModelAssembler.java
@@ -2,10 +2,12 @@ package com.github.brunoabdon.commons.rest.assembler;
 
 import com.github.brunoabdon.commons.modelo.Identifiable;
 import com.github.brunoabdon.commons.rest.modelfiller.ModelFiller;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 
+@Slf4j
 public abstract class IdentifiableModelAssembler<
     I extends Identifiable<?>,
     D extends RepresentationModel<?>>
@@ -22,6 +24,8 @@ public abstract class IdentifiableModelAssembler<
 
     @Override
     public D toModel(final I identifiable) {
+
+        log.debug("Transformando pra model o identifiable {}.",identifiable);
 
         final D model =
             createModelWithId(identifiable.getId(),identifiable);
