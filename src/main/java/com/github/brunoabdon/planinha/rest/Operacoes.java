fcd2d3cb84c93d70935c1d5bdc29571f1bfdbeca
bruno abdon <brunoabdon@github.com>
2021-09-18 03:02:25 -0300

    Usa assembler em Operacoes

--- src/main/java/com/github/brunoabdon/planinha/rest/Operacoes.java
+++ src/main/java/com/github/brunoabdon/planinha/rest/Operacoes.java
@@ -1,16 +1,25 @@
 package com.github.brunoabdon.planinha.rest;
 
-import static lombok.AccessLevel.PACKAGE;
-
-import java.time.LocalDate;
-import java.time.YearMonth;
-
+import com.github.brunoabdon.commons.facade.BusinessException;
+import com.github.brunoabdon.commons.facade.EntidadeInexistenteException;
+import com.github.brunoabdon.commons.facade.Facade;
+import com.github.brunoabdon.commons.modelo.Periodo;
+import com.github.brunoabdon.planinha.modelo.Operacao;
+import com.github.brunoabdon.planinha.rest.model.OperacaoModel;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,14 +29,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.github.brunoabdon.commons.facade.BusinessException;
-import com.github.brunoabdon.commons.facade.EntidadeInexistenteException;
-import com.github.brunoabdon.commons.facade.Facade;
-import com.github.brunoabdon.commons.modelo.Periodo;
-import com.github.brunoabdon.planinha.modelo.Operacao;
+import java.time.LocalDate;
+import java.time.YearMonth;
 
-import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
+import static lombok.AccessLevel.PACKAGE;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Slf4j
 @Setter(PACKAGE)
@@ -36,10 +43,16 @@ import lombok.extern.slf4j.Slf4j;
 public class Operacoes {
 
     @Autowired
+    private PagedResourcesAssembler<Operacao> pgAsmblr;
+
+    @Autowired
     private Facade<Operacao, Integer, Periodo, ?> facade;
 
+    @Autowired
+    private RepresentationModelAssembler<Operacao, OperacaoModel> operacaoAssembler;
+
     @GetMapping
-    public Page<Operacao> listar(
+    public ResponseEntity<PagedModel<OperacaoModel>> listar(
             @RequestParam(name="mes", required = false)
             final YearMonth mes,
 
@@ -67,16 +80,31 @@ public class Operacoes {
 
 		final Periodo periodo = periodoMes.intersecao(periodoDatas);
 
-		return facade.lista(periodo,pageable);
+        final Page<Operacao> page = facade.lista(periodo, pageable);
+
+        final Link self =
+            linkTo(
+                methodOn(Operacoes.class).listar(
+                    mes,dataMinima,dataMaxima, pageable
+                )
+            ).withSelfRel();
+
+        return
+            new ResponseEntity<>(
+                    pgAsmblr.toModel(page,operacaoAssembler, self),
+                    HttpStatus.OK
+            );
     }
 
     @GetMapping("{operacao_id}")
-    public Operacao pegar(@PathVariable("operacao_id") final Integer idOperacao)
+    public OperacaoModel pegar(@PathVariable("operacao_id") final Integer idOperacao)
             throws EntidadeInexistenteException {
 
         log.debug("Pegando operacao {}.",idOperacao);
 
-        return facade.pega(idOperacao);
+        final Operacao operacao = facade.pega(idOperacao);
+
+        return operacaoAssembler.toModel(operacao);
     }
 
     @DeleteMapping("{id}")
